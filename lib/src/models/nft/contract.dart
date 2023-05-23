5ae2e302e2723ed307e63c0bda850d4d46812e54
adgomam <saidul@adgorithmics.com>
2023-05-23 10:06:46 +0700

    Update contract model class.

--- lib/src/models/nft/contract.dart
+++ lib/src/models/nft/contract.dart
@@ -1,21 +1,28 @@
 class Contract {
   Contract({
+    this.id,
     this.address,
     this.name,
+    this.type,
   });
 
   Contract.fromJson(dynamic json) {
+    id = json['id'];
     address = json['address'];
     name = json['name'];
+    type = json['type'];
   }
-
+  String? id;
   String? address;
   String? name;
+  String? type;
 
   Map<String, dynamic> toJson() {
     final map = <String, dynamic>{};
+    map['id'] = id;
     map['address'] = address;
     map['name'] = name;
+    map['type'] = type;
     return map;
   }
 }
