77e658d295fa67dffab0f55a32103469072cd0f8
adgojoe <jmaulana@adgorithmics.com>
2023-08-03 16:47:40 +0700

    remove test for now

--- test/helper/test_helper.dart
+++ test/helper/test_helper.dart
@@ -1,12 +1,1 @@
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'package:http/http.dart';
-import 'package:mockito/annotations.dart';
 
-// command
-// dart run build_runner build
-
-@GenerateMocks([
-  Client,
-  FlutterSecureStorage,
-])
-void main() {}
