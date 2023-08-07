8092184bd91991d6b0764103701da4ada1de6e5f
bosta ginting <bosta@adgorithmics.com>
2023-08-07 09:40:48 +0700

    rollback the wallet address

--- example/lib/demo_features/nft/nft_controller.dart
+++ example/lib/demo_features/nft/nft_controller.dart
@@ -44,7 +44,7 @@ class NftController extends BaseListController {
     error = '';
     final resource = await _getNftListUseCase.invoke(
       page: page,
-      walletAddress: '0x93cd916bA08c905f4c05b6D3C71432F59205a787',
+      walletAddress: walletAddress!,
       status: 'minted, pending',
     );
 
